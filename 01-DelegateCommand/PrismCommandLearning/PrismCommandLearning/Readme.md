75ac5891b33852e4af178b08eaef36bb7c8006a3
rew asd <fork213@126.com>
2024-01-25 15:59:37 +0800

    增加了简单的delegatecommand学习

---
+++ 01-DelegateCommand/PrismCommandLearning/PrismCommandLearning/Readme.md
@@ -0,0 +1,3 @@
+﻿#### 注意：1. 绑定的属性用法与一般属性用法不同，要注意BindableBase的属性设置方法
+#### 2. DelegateCommand仅使用了简单的方法，还有其他方法要学习
+#### 3. 本内容仅针对MainWindow与MainWindowViewModel，如果是contentcontrol下的Region不适用
---
+++ 01-DelegateCommand/PrismCommandLearning/PrismCommandLearning/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Threading.Tasks;
+
+namespace PrismCommandLearning.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+        //注意里的BindableBase派生出的属性方法与propfull自动生成的不同
+        private string _currentTime;
+
+        public string CurrentTime
+        {
+            get { return _currentTime; }
+            set { SetProperty(ref _currentTime, value); }
+        }
+
+        public DelegateCommand BeginTimeTick { get; private set; }
+
+
+        public MainWindowViewModel()
+        {
+            CurrentTime = DateTime.Now.ToString();
+
+
+            //1.普通方法
+            //BeginTimeTick = new DelegateCommand(ShowTime);
+
+            //2.Task
+            BeginTimeTick = new DelegateCommand(async () => await TimeTickTask());
+
+            //3.Await
+            //BeginTimeTick = new DelegateCommand(awaitMethod);
+        }
+
+        private void ShowTime()
+        {
+            CurrentTime = DateTime.Now.ToString();
+        }
+
+        private Task TimeTickTask()
+        {
+            return TaskMethod();
+        }
+
+        private async Task TaskMethod()
+        {
+            while (true)
+            {
+                CurrentTime = DateTime.Now.ToString();
+                await Task.Delay(1000);
+            }
+        }
+
+        private async void awaitMethod()
+        {
+            while (true)
+            {
+                CurrentTime = DateTime.Now.ToString();
+                await Task.Delay(1000);
+            }
+        }
+
+    }
+}
