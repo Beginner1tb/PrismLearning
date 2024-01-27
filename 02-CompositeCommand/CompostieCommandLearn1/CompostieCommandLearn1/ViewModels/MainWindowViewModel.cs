540a5f47534003bfdb3870ff2b1972fad84770aa
rew asd <fork213@126.com>
2024-01-27 11:20:43 +0800

    增加了CompositeCommand学习工程

---
+++ 02-CompositeCommand/CompostieCommandLearn1/CompostieCommandLearn1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+
+namespace CompostieCommandLearn1.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _curTime;
+
+        public string CurTime
+        {
+            get { return _curTime; }
+            set { SetProperty(ref _curTime, value);
+               // TimeCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private string _messageYes;
+
+        public string MessageYes
+        {
+            get { return _messageYes; }
+            set { SetProperty(ref _messageYes, value); }
+        }
+
+        private bool kkk = true;
+
+        public CompositeCommand Btn1CompositeCommand { get; private set; }
+
+        //public DelegateCommand TimeCommand { get;private set;}
+
+        public MainWindowViewModel()
+        {
+            Btn1CompositeCommand = new CompositeCommand();
+            //TimeCommand = new DelegateCommand(ExcuteTime, CanExcuteTime);
+            Btn1CompositeCommand.RegisterCommand(new DelegateCommand(ExcuteTime, CanExcuteTime));
+            Btn1CompositeCommand.RegisterCommand(new DelegateCommand(ExcuteMessage, CanExcuteMessage));
+        }
+
+        private bool CanExcuteMessage()
+        {
+            return true;
+        }
+
+        private bool CanExcuteTime()
+        {
+            return true;
+        }
+
+        private void ExcuteMessage()
+        {
+            CurTime = DateTime.Now.ToString();
+        }
+
+        private void ExcuteTime()
+        {
+            MessageYes = "Yes";
+        }
+    }
+}
