62db2741ebe3f72ae85fa5068b192019cc99e4fc
rew asd <fork213@126.com>
2024-02-06 16:40:50 +0800

    增加了module学习

---
+++ 04-ModuleSetup/InitialModule1/InitialModule1/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using InitialModule1.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace InitialModule1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }
+    }
+}
