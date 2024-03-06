ee1fc14ba68d002d7ce40172742d36f681e40f6b
rew asd <fork213@126.com>
2024-03-06 17:21:30 +0800

    增加了属性改变回调方法的测试

---
+++ 05-PropertyChangeCallback/PropertyChangeCallback/PropertyChangeCallback/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Prism;
+using Prism.Mvvm;
+using System;
+using System.Diagnostics;
+
+namespace PropertyChangeCallback.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+
+
+
+        private bool _sasd;
+
+        public bool Sasd
+        {
+            get => _sasd;
+            //某种方式上已经实现了委托，不在需要IActiveAware接口的事件，注意value是此时Sasd绑定的值
+            set => SetProperty(ref _sasd, value, () => {
+                if (value)
+                    OnIsActive();
+                else
+                    OnIsNotActive();
+            });
+        }
+
+        private void OnIsNotActive()
+        {
+            Debug.WriteLine("333");
+        }
+
+        private void OnIsActive()
+        {
+            Debug.WriteLine("444");
+        }
+
+
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
---
+++ 05-PropertyChangeCallback/PropertyChangeCallback/PropertyChangeCallback/ViewModels/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using Prism;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace PropertyChangeCallback.ViewModels
+{
+    //使用IActiveAware接口方式，可以被替代
+    public abstract class ViewModelBase : BindableBase, IActiveAware
+    {
+        private bool _isActive;
+        public bool IsActive
+        {
+            get => _isActive;
+            set => SetProperty(ref _isActive, value, () => {
+                if (value)
+                    OnIsActive();
+                else
+                    OnIsNotActive();
+            });
+        }
+
+        public event EventHandler IsActiveChanged;
+
+        protected virtual void OnIsActive() { Debug.WriteLine("111"); }
+        protected virtual void OnIsNotActive() { Debug.WriteLine("222"); }
+    }
+}
