afd2cc621501bbccc2551ec0e045470cf97e404b
rew asd <fork213@126.com>
2024-03-25 17:42:07 +0800

    ViewLocator学习

--- 07-EventAggregator/EventAggregator1/EventAggregator1/App.xaml.cs
+++ 07-EventAggregator/EventAggregator1/EventAggregator1/App.xaml.cs
@@ -12,9 +12,11 @@ namespace EventAggregator1
     /// </summary>
     public partial class App : PrismApplication
     {
+        //代表初始时加载的窗口，注意必须时Window,不能使用UserControl
         protected override Window CreateShell()
         {
             return Container.Resolve<MainWindow>();
+            //return Container.Resolve<PrismWindow1>();
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
